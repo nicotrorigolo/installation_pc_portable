0759f7a42f82f33deb9351b5eee7590474523520
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2023-04-02 06:35:19 +0200

    Update installation_pc_portable_perso_linux.sh

--- installation_pc_portable_perso_linux.sh
+++ installation_pc_portable_perso_linux.sh
@@ -3,20 +3,25 @@
 echo "Donner acces a l'utilisateur"
 sudo chmod o+rx /home/$USER
 
-echo "Installer package d'installation"
-sudo apt install -y wget git nano curl
+echo "Installer les packages d'installations"
+sudo apt install -y wget gpg apt-transport-https git nano curl
 
 echo "Installer google chrome"
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
-&& apt-get install -y ./google-chrome-stable_current_amd64.deb
+&& sudo apt-get install -y ./google-chrome-stable_current_amd64.deb \
+&& rm -f google-chrome-stable_current_amd64.deb
 
 echo "Installer vscode"
-sudo apt install -y code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg \
+&& sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg \
+&& sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' \
+&& rm -f packages.microsoft.gpg \
+&& sudo apt update \
+&& sudo apt install -y code
 
 echo "Installer vagrant"
 sudo apt install -y vagrant
 
 echo "Installation de docker"
-sudo apt update
 sudo apt install -y docker.io
 sudo usermod -aG docker $USER
