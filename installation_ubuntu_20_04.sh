964c06b4804acfda78180b2a92aa45bf5bcfc0da
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2023-05-10 00:21:02 +0200

    Update installation_ubuntu_20_04.sh

--- installation_ubuntu_20_04.sh
+++ installation_ubuntu_20_04.sh
@@ -21,6 +21,9 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 
 echo "Installer vagrant"
 sudo apt install -y vagrant
+# Suite a une evolution de version de vagrant, il faut declarer les adresses ip que l'on souhaite creer
+sudo mkdir /etc/vbox/ \
+&& sudo echo * 10.0.0.0/20 > /etc/vbox/networks.conf
 
 echo "Installation de docker"
 sudo apt install -y docker.io
