3afed14e7db1d09eb5ea723dcdd100779b589227
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2023-06-08 22:36:55 +0200

    Update installation_ubuntu_20_04.sh

--- installation_ubuntu_20_04.sh
+++ installation_ubuntu_20_04.sh
@@ -68,7 +68,8 @@ sudo apt install -y build-essential \
 && NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" \
 && (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc \
 && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" \
-&& brew install glab
+&& brew install glab \
+&& eval "$(glab completion -s bash)"  # auto-completion de glab
 
 echo "Fin des installations"
 echo "Redemarrer le PC pour pouvoir initialiser docker"
