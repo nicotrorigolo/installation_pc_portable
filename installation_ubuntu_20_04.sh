ba7e6b5ab5ebfb73fe584aa069a0ad9d9bd08695
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2023-07-21 04:15:06 +0200

    Update installation_ubuntu_20_04.sh

--- installation_ubuntu_20_04.sh
+++ installation_ubuntu_20_04.sh
@@ -95,5 +95,10 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
+echo "Installation de kubernetes"
+sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+systemctl enable kubelet
+
+
 echo "Fin des installations"
 echo "Redemarrer le PC pour pouvoir initialiser docker"
