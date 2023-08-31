f4d9516b414643be350adab476b020baa44ec06d
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2023-09-01 01:35:37 +0200

    Update installation_ubuntu_20_04.sh

--- installation_ubuntu_20_04.sh
+++ installation_ubuntu_20_04.sh
@@ -126,8 +126,10 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-
-
+Installation de Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+&& echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list \
+&& sudo apt update && sudo apt install terraform
 
 
 
