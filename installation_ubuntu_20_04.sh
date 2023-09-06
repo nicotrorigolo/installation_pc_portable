fd0d2778de9ef941a186c86e1c423a5db2ddbaf1
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2023-09-07 01:42:07 +0200

    Update installation_ubuntu_20_04.sh

--- installation_ubuntu_20_04.sh
+++ installation_ubuntu_20_04.sh
@@ -133,7 +133,8 @@ chmod 700 get_helm.sh
 echo "Installation de Terraform"
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg \
 && echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list \
-&& sudo apt update && sudo apt install terraform
+&& sudo apt update \
+&& sudo apt install terraform
 
 echo "Verifier que kvm est installable sur le pc"
 grep -E -c "vmx|svm" /proc/cpuinfo \
