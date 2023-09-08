c9dea04ca496af64a6d1b803a666f52c4411e1e4
nicotrorigolo <88196777+nicotrorigolo@users.noreply.github.com>
2023-09-08 15:41:59 +0200

    Update installation_ubuntu_20_04.sh

--- installation_ubuntu_20_04.sh
+++ installation_ubuntu_20_04.sh
@@ -125,10 +125,11 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | TAG=v5.0.
 #######################################################################################################################################
 
 echo "Installation de Helm (Il faut que K8S ou K3D soit deja installe)"
-helm repo add bitnami https://charts.bitnami.com/bitnami
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null \
+&& sudo apt-get install apt-transport-https --yes \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list \
+&& sudo apt-get update \
+&& sudo apt-get install helm
 
 echo "Installation de Terraform"
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg \
